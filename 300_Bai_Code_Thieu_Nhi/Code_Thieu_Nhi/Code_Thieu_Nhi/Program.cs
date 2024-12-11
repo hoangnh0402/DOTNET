793556eee476d4ff2d750863a8883d220ab9cd8f
hoangnh4204 <hoangnh4204@gmail.com>
2024-12-12 02:25:49 +0700

    start

---
+++ 300_Bai_Code_Thieu_Nhi/Code_Thieu_Nhi/Code_Thieu_Nhi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_Thieu_Nhi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
